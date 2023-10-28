795094a7d84df732d6c71fd24468c3874e139221
abel <abel.araujo1976@gmail.com>
2023-10-28 17:48:08 +0200

    fig.update_layout(width=800, height=450)

--- cluster.py
+++ cluster.py
@@ -288,6 +288,6 @@ elif means == 4 :
 
 fig.update_geos(fitbounds="locations", visible=True)
 fig.update_layout(legend_title_text='Etiquetas', legend_title_side='top', title_pad_l=260, title_y=0.86)
-fig.update_layout(width=900, height=506)
+fig.update_layout(width=800, height=450)
 
 st.plotly_chart(fig)
